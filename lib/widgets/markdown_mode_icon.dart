1500207b8977867c0ce6098707d2269c36bb28d2
krawieck <fil.krawczyk@gmail.com>
2021-02-21 19:16:52 +0100

    switch to RotatedBox instead of Transform.rotate

--- lib/widgets/markdown_mode_icon.dart
+++ lib/widgets/markdown_mode_icon.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 /// shows either brush icon if fancy is true, or build icon if it's false
@@ -8,7 +6,7 @@ import 'package:flutter/material.dart';
 /// brush icon is rotated to look similarly to build icon
 Widget markdownModeIcon({bool fancy}) => fancy
     ? const Icon(Icons.build)
-    : Transform.rotate(
-        angle: pi / 2,
-        child: const Icon(Icons.brush),
+    : const RotatedBox(
+        quarterTurns: 1,
+        child: Icon(Icons.brush),
       );
