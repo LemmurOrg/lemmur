79e242de7bde46e59af880545bfc9af38c387237
shilangyu <xmarcinmarcin@gmail.com>
2021-11-25 23:55:36 +0100

    Remove column

--- lib/pages/modlog/modlog.dart
+++ lib/pages/modlog/modlog.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../l10n/l10n.dart';
 import '../../util/observer_consumers.dart';
-import '../../widgets/bottom_safe.dart';
 import '../../widgets/failed_to_load.dart';
 import 'modlog_page_store.dart';
 import 'modlog_table.dart';
@@ -16,64 +15,62 @@ class ModlogPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("$name's modlog")),
-      body: LayoutBuilder(
-        builder: (context, constraints) => SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(minHeight: constraints.maxHeight),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SizedBox.shrink(),
-                ObserverBuilder<ModlogPageStore>(
-                  builder: (context, store) {
-                    if (!store.hasNextPage) {
-                      return const Center(child: Text('no more logs to show'));
-                    }
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) => SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox.shrink(),
+                  ObserverBuilder<ModlogPageStore>(
+                    builder: (context, store) {
+                      if (!store.hasNextPage) {
+                        return const Center(
+                            child: Text('no more logs to show'));
+                      }
 
-                    return store.modlogState.map(
-                      loading: () => const Center(
-                        child: CircularProgressIndicator.adaptive(),
-                      ),
-                      error: (errorTerm) => Center(
-                        child: FailedToLoad(
-                          message: errorTerm.tr(context),
-                          refresh: store.fetchPage,
+                      return store.modlogState.map(
+                        loading: () => const Center(
+                          child: CircularProgressIndicator.adaptive(),
                         ),
-                      ),
-                      data: (modlog) => ModlogTable(modlog: modlog),
-                    );
-                  },
-                ),
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: ObserverBuilder<ModlogPageStore>(
-                        builder: (context, store) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            TextButton(
-                              onPressed: store.hasPreviousPage &&
-                                      !store.modlogState.isLoading
-                                  ? store.previousPage
-                                  : null,
-                              child: const Icon(Icons.skip_previous),
-                            ),
-                            TextButton(
-                              onPressed: store.hasNextPage &&
-                                      !store.modlogState.isLoading
-                                  ? store.nextPage
-                                  : null,
-                              child: const Icon(Icons.skip_next),
-                            ),
-                          ],
+                        error: (errorTerm) => Center(
+                          child: FailedToLoad(
+                            message: errorTerm.tr(context),
+                            refresh: store.fetchPage,
+                          ),
                         ),
+                        data: (modlog) => ModlogTable(modlog: modlog),
+                      );
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ObserverBuilder<ModlogPageStore>(
+                      builder: (context, store) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                            onPressed: store.hasPreviousPage &&
+                                    !store.modlogState.isLoading
+                                ? store.previousPage
+                                : null,
+                            child: const Icon(Icons.skip_previous),
+                          ),
+                          TextButton(
+                            onPressed: store.hasNextPage &&
+                                    !store.modlogState.isLoading
+                                ? store.nextPage
+                                : null,
+                            child: const Icon(Icons.skip_next),
+                          ),
+                        ],
                       ),
                     ),
-                    const BottomSafe(),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
